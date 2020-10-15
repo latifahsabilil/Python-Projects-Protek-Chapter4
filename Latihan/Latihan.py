02c695c13dd70cdc45bd6d5c4c88d3b19308c188
latifahsabilil <71626855+latifahsabilil@users.noreply.github.com>
2020-10-15 10:18:42 +0700

    Create Latihan.py

---
+++ Latihan/Latihan.py
@@ -0,0 +1,44 @@
+print("1.", end = " ")
+x = 200000
+y = 10000
+
+print("Rp.", end =" ")
+print(x + y*17 + y*5//6)
+
+print("2.", end = " ")
+x = 795
+y = 12
+
+print(x/y, end = " ")
+print("liter")
+
+print("3.", end = " ")
+x = 795
+y = 12
+z = 50
+
+print(x/y/z, end = " ")
+print("kali")
+
+print("4.", end = " ")
+w = 125
+x = 62
+y = 256
+z = 70
+
+print("pukul", end = " ")
+print(6.00+0.45+w//x+y//x)
+
+print("5.", end = " ")
+print('============== Program Membuat Diagram Jumlah Siswa =============='+'\n')
+
+lakiLaki = int(input('Berapa jumlah mahasiswa laki-laki di jurusan PTIK UNS ?'))
+perempuan = int(input('Berapa jumlah mahasiswa perempuan di jurusan PTIK UNS ?'))
+
+lk = int(lakiLaki/10)
+pr = int(perempuan/10)
+print('=============='+'\n')
+
+print('Laki-laki = '+'*' * lk)
+print('Perempuan = '+'*' * pr)
+
